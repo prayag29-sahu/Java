d385aa7821652b3571f490d8cb539943e1781798
Prayag Sahu <sahuprayag229@gmail.com>
2025-05-27 22:53:26 +0530

    array list string

---
+++ CollegeTraining/Day29 Collections/ArraylistString.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+class ArraylistString{
+    public static void main(String[] args) {
+        ArrayList<String> str = new ArrayList<>();
+        str.add("Good");
+        str.add("Morning");
+        str.add("I am");
+        str.add("Student");
+        System.out.println(str);
+    }
+}
+// [Good, Morning, I am, Student]
