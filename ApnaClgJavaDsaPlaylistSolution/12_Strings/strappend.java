232e40fd5a761bc4bee1e40cd8a3b2d3f065218b
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-26 18:51:40 +0530

    string append

---
+++ ApnaClgJavaDsaPlaylistSolution/12_Strings/strappend.java
@@ -0,0 +1,8 @@
+public class strappend {
+    public static void main(String[] args) {
+        String str = "Hello ";
+        str = str + "String";
+        System.out.println(str);
+    }
+}
+// Hello String
