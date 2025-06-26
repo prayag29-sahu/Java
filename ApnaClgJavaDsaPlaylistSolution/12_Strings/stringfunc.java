248570a0a897b3d74b4625da74f436e0c8b8d525
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-26 19:02:15 +0530

    string func

---
+++ ApnaClgJavaDsaPlaylistSolution/12_Strings/stringfunc.java
@@ -0,0 +1,9 @@
+public class stringfunc {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("hello string ");
+        str.append("Builder");
+        str.append(" Append");
+        System.out.println(str);
+    }
+}
+// hello string Builder Append
