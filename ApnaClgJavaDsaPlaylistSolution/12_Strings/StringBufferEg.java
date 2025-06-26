4eb42593b6fcc4182ed3c20930da42730750a149
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-26 18:53:34 +0530

    string buffer

---
+++ ApnaClgJavaDsaPlaylistSolution/12_Strings/StringBufferEg.java
@@ -0,0 +1,9 @@
+public class StringBufferEg {
+    public static void main(String[] args) {
+        StringBuffer strbuff = new StringBuffer("hello ");
+        strbuff.append("Buffer string");
+
+        System.out.println(strbuff);
+    }
+}
+// hello Buffer string
