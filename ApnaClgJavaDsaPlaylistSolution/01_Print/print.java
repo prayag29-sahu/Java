17c365f04afd9350658d3714ccd6534a7d288e57
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-27 17:42:06 +0530

    Print in next line

--- ApnaClgJavaDsaPlaylistSolution/01_Print/print.java
+++ ApnaClgJavaDsaPlaylistSolution/01_Print/print.java
@@ -1,6 +1,11 @@
 class print {
     public static void main(String arge[])
     {
-        System.out.println("Hello World");
+        System.out.println("Hello ");
+        System.out.println("Java");
     }
 }
+/*
+Hello 
+Java
+ */
