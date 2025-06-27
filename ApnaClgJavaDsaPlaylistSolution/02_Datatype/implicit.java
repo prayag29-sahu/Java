44d1e55c9e60fd50e07335fb5acd4f3ceee8c968
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-27 17:57:24 +0530

    implicit java

---
+++ ApnaClgJavaDsaPlaylistSolution/02_Datatype/implicit.java
@@ -0,0 +1,12 @@
+public class implicit {
+    public static void main(String[] args) {
+        int a = 10;
+        double d = a;
+        System.out.println(a);
+        System.out.println(d);
+    }
+}
+/*
+10
+10.0
+ */
