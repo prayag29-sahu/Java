dbea2b74737dd8299c6c7a27cb03c206f5200eb4
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-27 17:51:51 +0530

    input class

---
+++ ApnaClgJavaDsaPlaylistSolution/02_Datatype/inputclass.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class inputclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input an number : ");
+        int num = sc.nextInt();
+        System.out.println("Inputed number is : "+num);
+    }
+    
+}
+/*
+Input an number : 4
+Inputed number is : 4
+ */
