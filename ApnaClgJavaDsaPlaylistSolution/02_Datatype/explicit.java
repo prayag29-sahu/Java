b7b7b08092a8e42707347d2c74c537fcbde4344b
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-27 18:00:42 +0530

    explicit

---
+++ ApnaClgJavaDsaPlaylistSolution/02_Datatype/explicit.java
@@ -0,0 +1,12 @@
+public class explicit {
+    public static void main(String[] args) {
+        double d = 20.0;
+        int i = (int) d;
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
+/*
+20.0
+20
+ */
