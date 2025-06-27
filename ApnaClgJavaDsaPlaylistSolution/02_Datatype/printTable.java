ec4d850b711d591987308fa63bbcd2cd567bb2cc
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-27 18:03:39 +0530

    print Table

---
+++ ApnaClgJavaDsaPlaylistSolution/02_Datatype/printTable.java
@@ -0,0 +1,23 @@
+
+public class printTable {
+
+    public static void main(String[] args) {
+        System.out.println("Input Any Number To Generate A Table : ");
+        int number = 5;
+        System.out.print(number * 1 + " ");
+        System.out.print(number * 2 + " ");
+        System.out.print(number * 3 + " ");
+        System.out.print(number * 4 + " ");
+        System.out.print(number * 5 + " ");
+        System.out.print(number * 6 + " ");
+        System.out.print(number * 7 + " ");
+        System.out.print(number * 8 + " ");
+        System.out.print(number * 9 + " ");
+        System.out.print(number * 10 + " ");
+
+    }
+}
+/*
+Input Any Number To Generate A Table : 
+5 10 15 20 25 30 35 40 45 50
+ */
