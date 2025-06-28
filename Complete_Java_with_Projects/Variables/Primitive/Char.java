cde43a3b63841a2c8ead2e67e08756863b83a051
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-28 14:41:10 +0530

    Char

---
+++ Complete_Java_with_Projects/Variables/Primitive/Char.java
@@ -0,0 +1,16 @@
+public class Char {
+    public static void main(String[] args) {
+        
+        char ch1 = 'A';
+        System.out.println(ch1); // A
+        
+        char ch2 = '!';
+        System.out.println(ch2); // !
+        
+        char ch3 = '$';
+        System.out.println(ch3); // $
+        
+        char ch4 = '4';
+        System.out.println(ch4); // 4
+    }
+}
