595ab4ec0c51f4452ed4c2894e146972bc73676f
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-28 14:32:43 +0530

    Print in java

---
+++ Complete_Java_with_Projects/Intro/Print.java
@@ -0,0 +1,7 @@
+public class Print {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("Java\n");
+        System.out.println("Let's start");
+    }
+}
