825882150eac181bf8e7370331fc9be4542e9e00
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-28 13:19:01 +0530

    updations

--- ApnaClgJavaDsaPlaylistSolution/02_Datatype/hw.java
+++ ApnaClgJavaDsaPlaylistSolution/02_Datatype/hw.java
@@ -16,21 +16,6 @@ public class hw {
         float radius = sc.nextFloat();
         float area = 3.14f * radius * radius;
         System.out.println("Area Of Circle : "+area);
-
-        // 3> Print table of a Number 
-        System.out.println("Input Any Number To Generate A Table : ");
-        int number = sc.nextInt();
-        System.out.print(number*1+" ");
-        System.out.print(number*2+" ");
-        System.out.print(number*3+" ");
-        System.out.print(number*4+" ");
-        System.out.print(number*5+" ");
-        System.out.print(number*6+" ");
-        System.out.print(number*7+" ");
-        System.out.print(number*8+" ");
-        System.out.print(number*9+" ");
-        System.out.print(number * 10 + " ");
-
         sc.close();
     }
 }
