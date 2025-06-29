1fecc5a9f06255e5a310ad749b227fd9b2a296d7
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-29 13:35:33 +0530

    Details.java

---
+++ Complete_Java_with_Projects/Variables/Reference/Details.java
@@ -0,0 +1,11 @@
+public class Details {
+    public static void main(String[] args) {
+        String name = "Harry";
+        int age = 19;
+        String Course = "B.tech";
+        double cgpa= 8.81;
+        char grade = 'A';
+        System.out.println("Name : "+name+" age : "+age+" course : "+Course+" CGPA : "+cgpa+" grade : "+grade);
+        // Name : Harry age : 19 course : B.tech CGPA : 8.81 grade : A
+    }
+}
