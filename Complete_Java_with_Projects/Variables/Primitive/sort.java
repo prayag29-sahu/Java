350e335b9f6aaeee6f389e1629b01245a32f0cc1
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-29 13:16:00 +0530

    short

---
+++ Complete_Java_with_Projects/Variables/Primitive/sort.java
@@ -0,0 +1,14 @@
+public class sort {
+    public static void main(String[] args) {
+        
+        short s = 10000;
+        System.out.println(s);  // 10000
+        
+        // short s1 = 100000;
+        // System.out.println(s1);  // incompatible types: possible lossy conversion from int to short
+        
+        short s2 = -10000;
+        System.out.println(s2);  // -10000
+
+    }
+}
