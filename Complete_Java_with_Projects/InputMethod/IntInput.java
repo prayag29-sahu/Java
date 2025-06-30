b58b3a58a37dd36eb534423d61fc3f99eb0cd07b
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-30 23:02:24 +0530

    IntInput

---
+++ Complete_Java_with_Projects/InputMethod/IntInput.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class IntInput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
+        sc.close();
+    }
+
+}
+/*
+26
+26
+ */
