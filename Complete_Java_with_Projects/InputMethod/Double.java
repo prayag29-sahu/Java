07d61f21da7b0575e7821d3f8cedf04a98612adf
Prayag Sahu <sahuprayag229@gmail.com>
2025-06-30 23:00:50 +0530

    Double.java

---
+++ Complete_Java_with_Projects/InputMethod/Double.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Double {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+        System.out.println(d);
+        sc.close();
+    }
+}
+/*
+2445.244
+2445.244
+ */
