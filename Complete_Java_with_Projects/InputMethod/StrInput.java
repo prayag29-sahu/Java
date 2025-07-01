411f5ca5aafe67fed703dd52b60d645d1e7ab866
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-01 18:23:13 +0530

    String nextline

--- Complete_Java_with_Projects/InputMethod/StrInput.java
+++ Complete_Java_with_Projects/InputMethod/StrInput.java
@@ -5,11 +5,14 @@ public class StrInput {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         System.out.println(str);
+        String str1 = sc.nextLine();
+        System.out.println(str1);
         sc.close();
     }
     
 }
 /*
-hello java
-hello
+Hello
+Hello
+
  */
