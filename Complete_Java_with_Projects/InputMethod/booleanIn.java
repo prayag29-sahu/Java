8bed9cbda202c12a326e65fa9078041fcfa5727f
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-01 18:37:48 +0530

    boolean

---
+++ Complete_Java_with_Projects/InputMethod/booleanIn.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class booleanIn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean b = sc.nextBoolean();
+        System.out.println(b);
+        sc.close();
+
+    }
+}
+/*
+true
+true
+*/
