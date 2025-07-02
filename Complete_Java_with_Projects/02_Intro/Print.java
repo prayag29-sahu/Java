0385adf98301ac9bf6eeba808e29ae4839a962e5
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-02 18:15:17 +0530

    Delete Complete_Java_with_Projects/02_Intro directory

--- Complete_Java_with_Projects/02_Intro/Print.java
+++
@@ -1,7 +0,0 @@
-public class Print {
-    public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("Java\n");
-        System.out.println("Let's start");
-    }
-}
