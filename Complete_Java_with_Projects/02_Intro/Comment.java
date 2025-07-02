d323fe96c2a5ee6e5fded4daa7258068fa3ef27e
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-02 18:11:20 +0530

    Update file name

---
+++ Complete_Java_with_Projects/02_Intro/Comment.java
@@ -0,0 +1,15 @@
+public class Comment {
+    public static void main(String[] args) {
+
+ // its a single line commment
+
+        System.out.print("Hello ");
+        System.out.print("Java\n");
+        System.out.println("Let's start");
+    }
+}
+
+/* multiline comment
+Hello Java
+Let's start
+ */
