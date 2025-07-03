27972c64f9cec98772829ee7ec3bcbda8341d582
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-03 17:32:19 +0530

    Arithmetic Operators

---
+++ Complete_Java_with_Projects/04_Operators/Arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic{
+    public static void main(String[] args) {
+        int a=10;
+        int b=2;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
+/*
+Addition: 12
+Subtraction: 8
+Multiplication: 20
+Division: 5
+Modulus: 0
+ */
