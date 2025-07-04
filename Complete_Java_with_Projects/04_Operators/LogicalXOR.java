d54b90b31f8cc14cc2628880ea9122b96f320978
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-04 18:18:10 +0530

    Logical XOR oprt

---
+++ Complete_Java_with_Projects/04_Operators/LogicalXOR.java
@@ -0,0 +1,16 @@
+
+public class LogicalXOR {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+        boolean d = false;
+        System.out.println("a ^ b: " + (a ^ b)); // true
+        System.out.println("a ^ c: " + (a ^ c)); // false
+        System.out.println("a ^ d: " + (a ^ d)); // true
+        System.out.println("b ^ c: " + (b ^ c)); // true        
+        System.out.println("b ^ d: " + (b ^ d)); // false
+        System.out.println("c ^ d: " + (c ^ d)); // true
+    }
+}
