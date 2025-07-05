ba5fbd0214ba0f7f024238079a4487b70fa7cb16
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-05 11:54:57 +0530

    Bitwise NOR

---
+++ Complete_Java_with_Projects/04_Operators/BitwiseNOR.java
@@ -0,0 +1,9 @@
+
+public class BitwiseNOR {
+
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        System.out.println("a ^ b: " + (a ^ b)); // 6
+    }
+}
