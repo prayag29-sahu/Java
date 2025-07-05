3b53899fe447dce4b68b26f1fd686dabe5190d32
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-05 11:53:51 +0530

    Bitwise OR

---
+++ Complete_Java_with_Projects/04_Operators/BitwiseOR.java
@@ -0,0 +1,9 @@
+
+public class BitwiseOR {
+
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        System.out.println("a | b: " + (a | b)); // 7
+    }
+}
