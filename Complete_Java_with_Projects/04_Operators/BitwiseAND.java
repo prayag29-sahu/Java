07c62e812c1a5b08dc2e0787703a83ebafcdd3b2
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-05 11:38:42 +0530

    Bitwise AND

---
+++ Complete_Java_with_Projects/04_Operators/BitwiseAND.java
@@ -0,0 +1,10 @@
+
+public class BitwiseAND {
+
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        int b = 3;  // 0011
+        System.out.println("a & b: " + (a & b)); // 1
+        
+    }
+}
