f920a9af22fa2f7ddccf444813dbe7d1afbb2ddb
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-05 11:57:36 +0530

    Leftshift bits

---
+++ Complete_Java_with_Projects/04_Operators/Leftshift.java
@@ -0,0 +1,8 @@
+public class Leftshift {
+    public static void main(String[] args) {
+        int a = 5;  // 0101
+        System.out.println("a << 1: " + (a << 1)); // 10
+        System.out.println("Binary of " + a + " is: " + Integer.toBinaryString(a)); // 101
+        System.out.println("Binary of " + (a << 1) + " is: " + Integer.toBinaryString(a << 1)); // 1010
+    }
+}
