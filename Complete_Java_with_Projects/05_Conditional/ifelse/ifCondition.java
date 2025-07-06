bddadb044c62ff3aabd84a21b6b8db6c1cc64b00
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-06 12:50:32 +0530

    if optr

---
+++ Complete_Java_with_Projects/05_Conditional/ifelse/ifCondition.java
@@ -0,0 +1,15 @@
+
+public class ifCondition {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is less than or equal to b");
+        }
+        // a is less than or equal to b
+    }
+
+}
