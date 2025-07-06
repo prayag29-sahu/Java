55d2f430f4bc6115f33f5c1101f26ddc88491cac
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-06 12:44:23 +0530

    ternary optr

---
+++ Complete_Java_with_Projects/04_Operators/ternaryOpt.java
@@ -0,0 +1,8 @@
+public class ternaryOpt {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int max = (a > b) ? a : b;
+        System.out.println("Max: " + max); // Max: 10
+    }
+}
