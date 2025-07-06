2cf0c5764eaa7cd30ebe94a74dd56d70898a32fb
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-06 12:42:24 +0530

    comparison optr

---
+++ Complete_Java_with_Projects/04_Operators/comparison.java
@@ -0,0 +1,8 @@
+public class comparison {
+    public static void main(String[] args) {
+        int a = 5; // 0101
+        int b = 10; // 1010
+        System.out.println("a == b: " + (a == b)); // false 
+        System.out.println("a != b: " + (a != b)); // true
+}
+}
