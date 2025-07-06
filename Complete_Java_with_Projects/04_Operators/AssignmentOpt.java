b95ce750745baf1f55a18b055115daf6bc67e30c
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-06 12:41:37 +0530

    Assignment optr

---
+++ Complete_Java_with_Projects/04_Operators/AssignmentOpt.java
@@ -0,0 +1,10 @@
+public class AssignmentOpt {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        a += 5; // a = a + 5
+        b -= 3; // b = b - 3
+        System.out.println("a: " + a);// a: 10
+        System.out.println("b: " + b);// b: 7
+    }
+}
