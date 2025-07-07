4babf15a5f8fa0cd80586fa567da42f5170f6db3
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-07 14:08:56 +0530

    nested if

---
+++ Complete_Java_with_Projects/05_Conditional/ifelse/nestedif.java
@@ -0,0 +1,20 @@
+public class nestedif {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("Positive number");
+            if (num % 2 == 0) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        } else {
+            System.out.println("Negative number");
+        }
+    }
+}
+/*
+Positive number
+Even number
+ */
