1a7a12a9a652adfb3c63aec3c72eb41d20726505
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-07 13:57:58 +0530

    greater num if else

---
+++ Complete_Java_with_Projects/05_Conditional/ifelse/greaternum.java
@@ -0,0 +1,15 @@
+public class greaternum {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else if (a < b) {
+            System.out.println("a is less than b");
+        } else {
+            System.out.println("a is equal to b");
+        }
+        // a is greater than b
+    }
+}
