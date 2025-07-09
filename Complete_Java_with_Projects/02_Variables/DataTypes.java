66957551c0d09cbcd71731b14b90e3627e05c127
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-09 17:58:28 +0530

    DataTypes

---
+++ Complete_Java_with_Projects/02_Variables/DataTypes.java
@@ -0,0 +1,20 @@
+
+public class DataTypes {
+    public static void main(String[] args) {
+		int num1=9;
+		byte by=127;
+		short sh=558;
+		long l=5854l;
+		
+		float f=5.8f;
+		double d=5.8;
+		
+		char c='k';
+		
+		boolean b=true;
+
+        System.out.println(num1+" "+by+" "+sh+" "+l+" "+f+" "+d+" "+c+ " "+b);
+    }
+
+}
+// 9 127 558 5854 5.8 5.8 k true
