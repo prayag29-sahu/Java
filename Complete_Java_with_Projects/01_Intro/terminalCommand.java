125e70190a7a65772c490aa6af564586cad75d15
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-09 17:43:51 +0530

    terminal clas

---
+++ Complete_Java_with_Projects/01_Intro/terminalCommand.java
@@ -0,0 +1,18 @@
+public class terminalCommand {
+    
+}
+/*
+PS F:\Documents\java\Complete java\Java\Complete_Java_with_Projects\01_Intro>jshell  
+|  Welcome to JShell -- Version 24
+|  For an introduction type: /help intro
+
+jshell> System.out.print("Hello world")
+Hello world
+jshell> 2+4
+$2 ==> 6
+
+jshell> 34*2
+$3 ==> 68
+
+jshell>
+ */
