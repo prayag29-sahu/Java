898552c4b16ecd82dc78f9cb2c9381fbe46b77ab
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-11 15:35:25 +0530

    Modify

--- Complete_Java_with_Projects/05_Conditional/switchcase/checkInput.java
+++ Complete_Java_with_Projects/05_Conditional/switchcase/checkInput.java
@@ -1,31 +1,21 @@
 
 import java.util.Scanner;
 
-
 public class checkInput {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number between 1 and 5: ");
-        int input = sc.nextInt();
-
-        switch (input) {
-            case 1:
-                System.out.println("Input is 1");
-                break;
-            case 2:
-                System.out.println("Input is 2");
-                break;
-            case 3:
-                System.out.println("Input is 3");
-                break;
-            case 4:
-                System.out.println("Input is 4");
-                break;
-            case 5:
-                System.out.println("Input is 5");
-                break;
-            default:
-                System.out.println("Invalid input");
+        System.out.print("Enter a day: ");
+        String day = sc.nextLine();
+        switch (day) {
+            case "Monday" ->
+                System.out.println("Monday");
+            case "Tuesday" ->
+                System.out.println("Tuesday");
+            case "Saturday", "Sunday" ->
+                System.out.println("Holiday");
+            default ->
+                System.out.println("Enter a valid number");
         }
         sc.close();
     }
