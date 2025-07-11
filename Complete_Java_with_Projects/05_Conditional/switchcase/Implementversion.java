e9d737c4bb64c6e47bb8aea4d76f86366bec709f
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-11 15:36:39 +0530

    Implement version

---
+++ Complete_Java_with_Projects/05_Conditional/switchcase/Implementversion.java
@@ -0,0 +1,16 @@
+public class Implementversion {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        switch (Integer.compare(a, b)) {
+            case -1 ->
+                System.out.println("a is smaller than b");
+            case 1 ->
+                System.out.println("b is smaller than a");
+            default ->
+                System.out.println("a is equal to b");
+        }
+    }
+}
+// a is smaller than b
