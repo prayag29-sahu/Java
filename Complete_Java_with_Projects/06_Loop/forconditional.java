595121d8860427266967f657f0b417aea2ac3476
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-12 15:55:01 +0530

    for decleration

---
+++ Complete_Java_with_Projects/06_Loop/forconditional.java
@@ -0,0 +1,30 @@
+
+public class forconditional {
+    public static void main(String[] args) {
+		for(int i=0;i<=4;i++)
+		{
+			System.out.println("Hi"+i);
+		}
+		
+		
+		int i=1;
+		for(;i<=5;)
+		{
+			System.out.println("DAY"+i);
+			i++;
+		}
+	}
+
+}
+/*
+Hi0
+Hi1
+Hi2
+Hi3
+Hi4
+DAY1
+DAY2
+DAY3
+DAY4
+DAY5
+ */
