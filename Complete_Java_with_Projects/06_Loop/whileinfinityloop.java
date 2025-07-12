469f19da5d16c403b916ab62f6062d620425f332
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-12 15:51:08 +0530

    while linfinite

---
+++ Complete_Java_with_Projects/06_Loop/whileinfinityloop.java
@@ -0,0 +1,12 @@
+public class whileinfinityloop {
+    public static void main(String[] args) {
+		int i=1;
+		
+		while(true) // infinity loop
+		{
+			System.out.println("Hi"+ i);
+			i++;
+		}
+    }
+
+}
