10d2fb218471075817d89444215e2b82cd3a0f24
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-12 15:53:30 +0530

    for loop

---
+++ Complete_Java_with_Projects/06_Loop/forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Iteration: " + i);
+        }
+    }
+}
+/*
+Iteration: 0
+Iteration: 1
+Iteration: 2
+Iteration: 3
+Iteration: 4
+ */
