84bd6403013e630cffa58449d7f72b2b5f2718a0
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-12 15:50:37 +0530

    whileloop

---
+++ Complete_Java_with_Projects/06_Loop/whileloop.java
@@ -0,0 +1,17 @@
+
+public class whileloop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Count: " + i);
+            i++;
+        }
+    }
+}
+/*
+Count: 1
+Count: 2
+Count: 3
+Count: 4
+Count: 5
+ */
