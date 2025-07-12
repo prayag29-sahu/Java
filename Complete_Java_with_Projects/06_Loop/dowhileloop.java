3ccbf09608139861e73e0ca32a93536e2e98f74c
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-12 15:52:43 +0530

    do while loop

---
+++ Complete_Java_with_Projects/06_Loop/dowhileloop.java
@@ -0,0 +1,25 @@
+
+public class dowhileloop {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Hi" + i);
+            i++;
+        } while (i <= 4);
+        /*
+Hi1
+Hi2
+Hi3
+Hi4
+         */
+        do {
+            System.out.println("Hi" + i);
+            i++;
+        } while (i <= 4);
+    }
+    /*
+Hi5
+     */
+
+}
