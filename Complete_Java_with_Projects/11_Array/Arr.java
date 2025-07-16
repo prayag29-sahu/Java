2c4900f84c24394f75357577f18deec52024a2a2
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-16 12:51:05 +0530

    Array init

---
+++ Complete_Java_with_Projects/11_Array/Arr.java
@@ -0,0 +1,13 @@
+
+public class Arr {
+
+    public static void main(String[] args) {
+        int nums[] = {3, 7, 2, 4};
+
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+
+    }
+}
