c15fef372961bcfda04afec79e0dfe3bd7689af2
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-16 12:52:06 +0530

    Array index

---
+++ Complete_Java_with_Projects/11_Array/Arr1.java
@@ -0,0 +1,16 @@
+
+public class Arr1 {
+
+    public static void main(String[] args) {
+
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+    }
+}
