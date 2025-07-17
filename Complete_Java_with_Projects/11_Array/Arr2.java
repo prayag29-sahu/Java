b61fc3e488bfdf24efc2e679b22cc5f986a02fc2
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-17 11:36:30 +0530

    Array init

---
+++ Complete_Java_with_Projects/11_Array/Arr2.java
@@ -0,0 +1,13 @@
+
+public class Arr2 {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+        for(int i=0;i<4;i++){
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
