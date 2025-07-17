605f0abd48f64ce5ca77105c35bc3c3480d5120e
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-17 11:38:16 +0530

    Array init with random

---
+++ Complete_Java_with_Projects/11_Array/Arr3.java
@@ -0,0 +1,11 @@
+
+public class Arr3 {
+        public static void main(String[] args) {
+        int[] a=new int[5];
+        for(int n : a){
+            n = (int)(Math.random()*100);
+            System.out.print(n+" ");
+        }
+    }
+}
+// 7 61 12 88 89 
