78ea5d5e038f47a15ffe86b0d347ee29d63626de
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-18 11:36:23 +0530

    Immutable string

---
+++ Complete_Java_with_Projects/14_String/UnmutableStr.java
@@ -0,0 +1,12 @@
+
+public class UnmutableStr {
+
+    public static void main(String[] args) {
+        String name = "Prayag";
+        name = name + " Sahu";
+        System.out.println("hello " + name);
+    }
+}
+/*
+hello Prayag Sahu
+ */
