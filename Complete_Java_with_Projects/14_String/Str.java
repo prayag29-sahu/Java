592910fa6f26b93909db962afc3880f18407e0d9
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-18 11:34:30 +0530

    String

---
+++ Complete_Java_with_Projects/14_String/Str.java
@@ -0,0 +1,17 @@
+public class Str {
+	public static void main(String[] args) 
+	{
+		String name=new String();
+		System.out.println(name);
+		System.out.println(name.hashCode());
+		System.out.println("hello "+name);
+		name="Prayag ";
+		System.out.println(name);
+		
+	}
+}
+/*
+0
+hello
+Prayag
+ */
