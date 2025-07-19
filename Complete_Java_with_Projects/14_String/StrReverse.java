7bb1f8e9119eeea000ba49c2565d8aef29cccd00
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-19 20:20:55 +0530

    String reverse

---
+++ Complete_Java_with_Projects/14_String/StrReverse.java
@@ -0,0 +1,11 @@
+
+public class StrReverse {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Rohan");
+
+        sb.reverse();
+        System.out.println(sb);
+
+    }
+}
