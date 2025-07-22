8deaf38cf2f9db8feb0cbbd3069f790aff0eea07
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-22 12:30:13 +0530

    Constructor

---
+++ Complete_Java_with_Projects/17_Constructor/Constructor.java
@@ -0,0 +1,15 @@
+
+class Human {
+
+    public Human() {
+        System.out.println("in constructor");
+    }
+
+}
+
+public class Constructor {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Human obj = new Human();
+    }
+}
