290d0f1644f07a71f2db56a032ed4c26b1135c47
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-23 11:47:12 +0530

    Parameterised constructor

---
+++ Complete_Java_with_Projects/17_Constructor/ParaConst.java
@@ -0,0 +1,14 @@
+class Human {
+
+    public Human(int a){
+        System.out.println(a+" Parameterised constructor");
+    }
+
+}
+
+public class ParaConst {
+    public static void main(String[] args) {
+        Human h1 = new Human(6);
+        
+    }
+}
