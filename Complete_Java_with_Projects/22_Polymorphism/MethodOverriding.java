146a667044cb7eb6f3bb4d426aedd5211733f99d
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-28 12:48:07 +0530

    method overloading

---
+++ Complete_Java_with_Projects/22_Polymorphism/MethodOverriding.java
@@ -0,0 +1,42 @@
+
+class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double add(double a, int b, int c) {
+        return a + b + c;
+    }
+
+}
+
+class AdvCalcu {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public double add(double a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class MethodOverriding {
+
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        AdvCalcu adv = new AdvCalcu();
+        int addition = cal.add(2, 4);
+        System.out.println(addition);
+        addition = cal.add(2, 4, 5);
+        System.out.println(addition);
+        addition = adv.add(2, 5);
+        System.out.println(addition);
+        double res = cal.add(5, 2, 7);
+        System.out.println(res);
+    }
+}
