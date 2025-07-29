0a2e4b5de10d53431857431dd36a28503b1e73d0
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-29 18:57:33 +0530

    Demo

---
+++ Complete_Java_with_Projects/20_Packages/Demo.java
@@ -0,0 +1,10 @@
+import Java.Complete_Java_with_Projects.20_Packages.tools.Calc;
+import Java.Complete_Java_with_Projects.20_Packages.tools.AdvCalc;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        Calc calculator = new Calc();
+        AdvCalc advancedCalculator = new AdvCalc();
+    }
+}
