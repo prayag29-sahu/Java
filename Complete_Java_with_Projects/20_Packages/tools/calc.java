fb22c6216122afb71a96e8fd8772995472610e62
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-29 18:59:34 +0530

    Calculator

---
+++ Complete_Java_with_Projects/20_Packages/tools/calc.java
@@ -0,0 +1,10 @@
+package Java.Complete_Java_with_Projects.20_Packages.tools;
+
+class calc {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
