8ba397eaea70c03c3f51fa115e907144cba4df03
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-30 12:18:54 +0530

    Anonymous inner class

---
+++ Complete_Java_with_Projects/25_InnerClass/AnonymousInnerClass.java
@@ -0,0 +1,28 @@
+
+class A {
+
+    public void show() {
+        System.out.println("in A show");
+    }
+}
+//class B extends A
+//{
+//	public void show()
+//	{
+//		System.out.println("in B Show");
+//	}
+//}
+
+public class AnonymousInnerClass {
+
+    public static void main(String[] args) {
+
+        //A obj=new B();
+        A obj = new A() {
+            public void show() {
+                System.out.println("in new show");
+            }
+        };
+        obj.show();
+    }
+}
