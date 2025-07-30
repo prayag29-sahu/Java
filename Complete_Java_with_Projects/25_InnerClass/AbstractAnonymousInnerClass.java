d5ce88aad7992a829e8963971953f05d57a93559
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-30 12:24:03 +0530

    Abstract Anonymous inner

---
+++ Complete_Java_with_Projects/25_InnerClass/AbstractAnonymousInnerClass.java
@@ -0,0 +1,34 @@
+
+abstract class A {
+
+    public abstract void show();
+
+    public abstract void config();
+
+}
+
+//class B extends A
+//{
+//	public void show()
+//	{
+//		System.out.println("in B show");
+//	}
+//}
+public class AbstractAnonymousInnerClass {
+
+    public static void main(String[] args) {
+
+//    	A obj=new B();
+        A obj = new A() {
+            public void show() {
+                System.out.println("in new show");
+            }
+            public void config() {
+                System.out.println("in new config");
+            }
+        };
+        obj.show();
+        obj.config();
+    
+    }
+}
