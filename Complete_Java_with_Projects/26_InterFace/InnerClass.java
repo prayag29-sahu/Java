c5e5dd756b5e6bc48046f2de5229c0920cd8d228
Prayag Sahu <sahuprayag229@gmail.com>
2025-07-31 12:32:01 +0530

    Inner class

---
+++ Complete_Java_with_Projects/26_InterFace/InnerClass.java
@@ -0,0 +1,29 @@
+
+@FunctionalInterface
+interface A {
+
+    void show();
+//	void run();
+}
+//class B implements A
+//{
+//	public void show()
+//	{
+//		System.out.println("in Show");
+//	}
+//}
+
+public class InnerClass {
+
+    public static void main(String[] args) {
+
+        A obj = new A() {
+            public void show() {
+                System.out.println("in Show");
+            }
+        };
+        //   	A obj=new A();
+        //   	A obj=new B();
+        obj.show();
+    }
+}
