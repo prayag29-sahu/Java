dcb15b42f2a267dc5bbe2cc27706b1bbf66a66da
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-04 13:40:06 +0530

    Usig Scanner

---
+++ Complete_Java_with_Projects/31_UserInput/UsingScanner.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class UsingScanner {
+
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an number : ");
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
