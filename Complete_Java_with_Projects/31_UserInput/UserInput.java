3a1092475378f1cb1f2f4522f8147310c2e3d3ae
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-04 13:38:13 +0530

    User Input

---
+++ Complete_Java_with_Projects/31_UserInput/UserInput.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static void main(String[] args) throws IOException {
+
+//    	System.out.println("Enter a number");
+//    	int num=System.in.read();
+//    	
+//  	System.out.println(num);
+//    	System.out.println(num-48);
+        System.out.println("Enter a number");
+
+//    	InputStreamReader in=new InputStreamReader(System.in);
+//    	BufferedReader bf=new BufferedReader(in);
+//    	int num=Integer.parseInt(bf.readLine());
+//    	System.out.println(num);
+        //  	BufferedReader bf=new BufferedReader(null);
+        // 	System.out.println(num-48);
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
