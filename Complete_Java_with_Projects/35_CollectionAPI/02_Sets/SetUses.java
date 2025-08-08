3e192a876ef33856342101c7ac3a87145440f34e
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-08 19:01:24 +0530

    Uses of set

---
+++ Complete_Java_with_Projects/35_CollectionAPI/02_Sets/SetUses.java
@@ -0,0 +1,26 @@
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class SetUses {
+
+    public static void main(String[] args) {
+
+        Set<Integer> nums = new HashSet<Integer>();
+
+        nums.add(62);
+        nums.add(54);
+        nums.add(82);
+        nums.add(21);
+
+        //nums.add("5");
+        Iterator<Integer> values = nums.iterator();
+
+        while (values.hasNext()) {
+            System.out.println(values.next());
+        }
+
+    }
+}
