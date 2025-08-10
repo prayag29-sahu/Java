e4aaed493f07fdbc4b89508d986d950255ee6809
Prayag Sahu <sahuprayag229@gmail.com>
2025-08-10 21:10:09 +0530

    Super

---
+++ Complete_Java_with_Projects/23_FinalKeyword/SuperKeyword.java
@@ -0,0 +1,48 @@
+
+class Parent {
+
+    protected int num = 1;
+
+    Parent() {
+        System.out.println("Parent class default constructor.");
+    }
+
+    Parent(String x) {
+        System.out.println("Parent class parameterised constructor.");
+    }
+
+    public void foo() {
+        System.out.println("Parent class foo!");
+    }
+}
+
+class Child extends Parent {
+
+    private int num = 2;
+
+    Child() {
+        System.out.println("Child class default Constructor");
+        // super(); // to call default parent constructor
+        // super("Call Parent"); // to call parameterised constructor.
+    }
+
+    void printNum() {
+        System.out.println(num);
+        System.out.println(super.num); //prints the value of num of parent class
+    }
+
+    @Override
+    public void foo() {
+        System.out.println("Parent class foo!");
+        super.foo(); //Calls foo method of Parent class inside the Overriden foo
+    }
+}
+
+public class SuperKeyword {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.printNum();
+        child.foo();
+    }
+}
